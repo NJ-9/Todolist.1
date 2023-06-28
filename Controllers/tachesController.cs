cac5d1e8133e5e8dfef6be304f6ca2b5c8bf4005
NJ-9 <98019614+NJ-9@users.noreply.github.com>
2023-06-28 11:34:57 +0200

    Initial commit

---
+++ Controllers/tachesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Todolist._1;
+
+namespace Todolist._1.Controllers
+{
+    public class tachesController : Controller
+    {
+        private todolist_dbEntities db = new todolist_dbEntities();
+
+        // GET: taches
+        public ActionResult Index()
+        {
+            return View(db.tache.ToList());
+        }
+
+        // GET: taches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tache tache = db.tache.Find(id);
+            if (tache == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tache);
+        }
+
+        // GET: taches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: taches/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nom_de_l_élément,date,Est_terminé")] tache tache)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tache.Add(tache);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tache);
+        }
+
+        // GET: taches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tache tache = db.tache.Find(id);
+            if (tache == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tache);
+        }
+
+        // POST: taches/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nom_de_l_élément,date,Est_terminé")] tache tache)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tache).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tache);
+        }
+
+        // GET: taches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tache tache = db.tache.Find(id);
+            if (tache == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tache);
+        }
+
+        // POST: taches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tache tache = db.tache.Find(id);
+            db.tache.Remove(tache);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
